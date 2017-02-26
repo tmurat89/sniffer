3ea2d13b559049b528dad9dc1d84ea0a8038f3c0
Tomislav Murat <tomislav.murat@gmail.com>
2017-02-26 22:56:05 +0100

    autentikacija zavrsena

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,12 +9,14 @@ const LoginForm = ({
   onSubmit,
   onChange,
   errors,
+  successMessage,
   user
 }) => (
   <Card className="container">
     <form action="/" onSubmit={onSubmit}>
       <h2 className="card-heading">Login</h2>
 
+      {successMessage && <p className="success-message">{successMessage}</p>}
       {errors.summary && <p className="error-message">{errors.summary}</p>}
 
       <div className="field-line">
@@ -42,7 +44,7 @@ const LoginForm = ({
         <RaisedButton type="submit" label="Log in" primary />
       </div>
 
-      <CardText>Dont have an account? <Link to={'/signup'}>Create one</Link>.</CardText>
+      <CardText>Don't have an account? <Link to={'/signup'}>Create one</Link>.</CardText>
     </form>
   </Card>
 );
@@ -51,6 +53,7 @@ LoginForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   onChange: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
+  successMessage: PropTypes.string.isRequired,
   user: PropTypes.object.isRequired
 };
 
